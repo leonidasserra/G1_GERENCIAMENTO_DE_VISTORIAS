8336f6ec56c64093bb234d50995f42e4489f3c2e
Leonidas <leonidas.serra16@gmail.com>
2025-02-14 13:35:36 -0300

    Atualização requirements e views

--- codigo/app/app.py
+++ codigo/app/app.py
@@ -44,7 +44,7 @@ def create_app():
     app.config['SESSION_USE_SIGNER'] = True
     app.config['SECRET_KEY'] = 'chave_super_segura'
     app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'static', 'uploads')
-    app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024  # Exemplo: limita tamanho a 5MB
+    app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024  #Limita tamanho a 5MB
     Session(app)
 
     return app
